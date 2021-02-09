1fbb8c8449b9415cdec8f734d8fa3bff7ddac001
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2021-02-09 18:12:03 -0500

    Add files via upload
    upload tic-tac-toe files

---
+++ TicTacToe.java
@@ -0,0 +1,14 @@
+package lab1;
+
+/** A tic-tac-toe game for two players.
+ * @author Felix Lazo
+ */
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+		TicTacToeGame newGame = new TicTacToeGame();
+		newGame.beginGame();
+		
+	}
+}
